bfed16866a1bdfc85ab1bfe4cf2c111aa92760d5
Kim9xd <kim9xd@gmail.com>
2022-01-11 17:03:20 +0900

    git init

---
+++ src/main/java/study/querydsl/DesignPatternsPulpury1Application.java
@@ -0,0 +1,13 @@
+package study.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsPulpury1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsPulpury1Application.class, args);
+	}
+
+}
