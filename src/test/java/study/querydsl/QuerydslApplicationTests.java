732be9312659a7f022b715888e60a60f92354db3
Kim9xd <kim9xd@gmail.com>
2022-01-12 17:28:56 +0900

    * 프로젝트 환경설정    - Querydsl 설정과 검증

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,45 @@
 package study.querydsl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.persistence.EntityManager;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+	
+	@Autowired
+//	@PersistenceContext
+	EntityManager em;
 
 	@Test
-	void contextLoads() {
+	void contextLoads()  {
+		Hello hello = new Hello();
+		em.persist(hello);
+		
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		
+//		QHello qHello = new QHello("h");
+		// 위 혹은 아래처럼 사용할 수 있다.
+		QHello qHello = QHello.hello;
+		
+		
+		Hello result = query
+			.selectFrom(qHello)
+			.fetchOne();
+		
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
