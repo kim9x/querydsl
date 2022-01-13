d86f56aef88f6307a0d3172db4eeb9a6c0acaf6c
Kim9xd <kim9xd@gmail.com>
2022-01-13 21:54:51 +0900

    * 기본 문법    - Case 문 (이전 커밋, 잘못 push함)    - 상수, 문자 더하기

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -22,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import study.querydsl.entity.Member;
@@ -468,6 +469,35 @@ public class QuerydslBasicTest {
 							.when(member.age.between(21, 30)).then("21~30살")
 							.otherwise("기타"))
 				.from(member)
+				.fetch();
+		
+		for (String s : result) {
+			System.out.println("s = " + s);
+		}
+	}
+	
+	@Test
+	public void constant() {
+		List<Tuple> result = queryFactory
+				.select(member.username, Expressions.constant("A"))
+				.from(member)
+				.fetch();
+		
+		for (Tuple tuple : result) {
+			System.out.println("tuple = " + tuple);
+		}
+	}
+	
+	@Test
+	public void concat() {
+		
+		// {username}_{age}
+		// age가 숫자 타입이기 때문에 오류가 생긴다.
+		// 그러나 아래처럼 '.stringValue()'를 써주면 문제를 해결할 수 있다.
+		List<String> result = queryFactory
+				.select(member.username.concat("_").concat(member.age.stringValue()))
+				.from(member)
+				.where(member.username.eq("member1"))
 				.fetch();
 		
 		for (String s : result) {
