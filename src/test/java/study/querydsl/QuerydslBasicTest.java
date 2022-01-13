351cf71b98e5017f47240c0740ce099cbe3266b2
Kim9xd <kim9xd@gmail.com>
2022-01-13 21:46:52 +0900

    * 기본 문법    - 상수, 문자 더하기

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -21,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import study.querydsl.entity.Member;
@@ -434,7 +435,7 @@ public class QuerydslBasicTest {
 		List<Tuple> result = queryFactory
 				.select(member.username,
 						select(memberSub.age.avg())
-						.from(memberSub)) 
+						 .from(memberSub)) 
 				.from(memberSub)
 				.fetch();
 		
@@ -444,5 +445,34 @@ public class QuerydslBasicTest {
 		
 	}
 	
+	@Test
+	public void basicCase() {
+		List<String> result = queryFactory
+				.select(member.age
+						.when(10).then("열살")
+						.when(20).then("스무살")
+						.otherwise("기타"))
+				.from(member)
+				.fetch();
+		
+		for (String s : result) {
+			System.out.println("s = " + s);
+		}
+	}
+	
+	@Test
+	public void complexCase() {
+		List<String> result = queryFactory
+				.select(new CaseBuilder()
+							.when(member.age.between(0, 20)).then("0~20살")
+							.when(member.age.between(21, 30)).then("21~30살")
+							.otherwise("기타"))
+				.from(member)
+				.fetch();
+		
+		for (String s : result) {
+			System.out.println("s = " + s);
+		}
+	}
 
 }
