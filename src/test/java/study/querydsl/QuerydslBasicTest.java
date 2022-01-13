772debfb5840e89dfebc3f0e31f22421630853e9
Kim9xd <kim9xd@gmail.com>
2022-01-13 15:55:23 +0900

    * 기본 문법    - 결과 조회    - 정렬

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -3,6 +3,8 @@ package study.querydsl;
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import org.assertj.core.api.Assertions;
@@ -12,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import study.querydsl.entity.Member;
@@ -100,6 +103,61 @@ public class QuerydslBasicTest {
 			.fetchOne();
 		
 		assertThat(findMember.getUsername()).isEqualTo("member1");
+	}
+	
+	@Test
+	public void resultFetch() {
+		queryFactory = new JPAQueryFactory(em);
+		
+		List<Member> fetch = queryFactory
+			.selectFrom(member)
+			.fetch();
+		
+//		Member fetchOne = queryFactory
+//			.selectFrom(member)
+//			.fetchOne();
+//		
+//		Member fetchFirst = queryFactory
+//			.selectFrom(member)
+//			.fetchFirst();
+			// '.fetchFirst()'은 아래에 위치한
+			// '.limit(1).fetchOne()'와 같다
+//			.limit(1).fetchOne();
+		
+		QueryResults<Member> results = queryFactory
+			.selectFrom(member)
+			.fetchResults();
+		
+		results.getTotal();
+		List<Member> content = results.getResults();
+	}
+	
+	/**
+	 * 회원 정렬 순서
+	 * 1. 회원 나이 내림차순(desc)
+	 * 2. 회원 이름 올림차순(asc)
+	 * 단 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+	 */
+	@Test
+	public void sort() {
+		queryFactory = new JPAQueryFactory(em);
+		
+		em.persist(new Member(null, 100));
+		em.persist(new Member("member5", 100));
+		em.persist(new Member("member6", 100));
+		
+		List<Member> result = queryFactory
+			.selectFrom(member)
+			.where(member.age.eq(100))
+			.orderBy(member.age.desc(), member.username.asc().nullsLast())
+			.fetch();
+		
+		 Member member5 = result.get(0);
+		 Member member6 = result.get(1);
+		 Member memberNull = result.get(2);
+		 assertThat(member5.getUsername()).isEqualTo("member5");
+		 assertThat(member6.getUsername()).isEqualTo("member6");
+		 assertThat(memberNull.getUsername()).isNull();
 	}
 	
 	
