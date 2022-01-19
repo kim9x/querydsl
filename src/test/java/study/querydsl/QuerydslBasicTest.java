ef0e3afaa5e36b36e998ee7e7a9dca1344887a9b
Kim9xd <kim9xd@gmail.com>
2022-01-19 18:16:24 +0900

    * 중급 문법    - 동적 쿼리 - BooleanBuilder 사용

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
@@ -621,7 +622,7 @@ public class QuerydslBasicTest {
 		}
 	}
 	
-	@Test
+	/*
 	public void findDtoByQueryProjection() {
 		List<MemberDto> result = queryFactory
 				 .select(new QMemberDto(member.username, member.age))
@@ -631,6 +632,32 @@ public class QuerydslBasicTest {
 		for (MemberDto memberDto : result) {
 			System.out.println("memberDto = " + memberDto);
 		}
+	}
+	*/
+	
+	@Test
+	public void dynamicQuery_BooleanBuilder() {
+		String usernameParam = "member1";
+		Integer ageParam = 10;
+		
+		List<Member> result = searchMember1(usernameParam, ageParam);
+		assertThat(result.size()).isEqualTo(1);
+	}
+
+	private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+		BooleanBuilder builder = new BooleanBuilder();
+		if (usernameCond != null) {
+			builder.and(member.username.eq(usernameCond));
+		}
+		
+		if (ageCond != null) {
+			builder.and(member.age.eq(ageCond));
+		}
+		
+		return queryFactory
+			.selectFrom(member)
+			.where(builder)
+			.fetch();
 	}
 
 }
